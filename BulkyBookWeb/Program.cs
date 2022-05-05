b7bd325155c27a5c66f904bde65f235a74202983
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-05-05 15:58:37 +0300

    Configure Application Cookie

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -27,6 +27,14 @@ builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
 builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.LogoutPath = "/Identity/Account/Logout";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
