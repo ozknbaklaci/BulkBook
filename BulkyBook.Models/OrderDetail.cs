bd795c9650b5744dc327afba86e0999792037c7a
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-05-06 14:51:04 +0300

    Create Order Header and Details Model and migrations

---
+++ BulkyBook.Models/OrderDetail.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BulkyBook.Models
+{
+    public class OrderDetail : BaseEntity
+    {
+        [Required]
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+
+        public double Price { get; set; }
+    }
+}
