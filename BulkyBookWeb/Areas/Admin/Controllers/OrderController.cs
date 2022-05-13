bc5d4ad5e94b67af2b2fbebc4dc87a444a855c9a
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-05-13 12:51:17 +0300

    OrderVM and Retrieve all Orders

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHeaderRepository;
+
+        public OrderController(IOrderHeaderRepository orderHeaderRepository)
+        {
+            _orderHeaderRepository = orderHeaderRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders = await _orderHeaderRepository.GetAllAsync(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
