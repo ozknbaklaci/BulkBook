51a8636131713b136dfbf71679799a8cc495a1e1
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-05-16 15:10:05 +0300

    mail Sender using MimeKit and MailKit

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace BulkyBook.Utility
 {
@@ -6,6 +8,21 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("test1.gmail.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                client.Authenticate("test1.gmail.com", "test1");
+                client.Send(emailToSend);
+                client.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
